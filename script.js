82065db5e81a125df496573756b37d0d0fa056d7
kevindiazistea <kevin.diaz@istea.com.ar>
2025-10-24 19:44:30 -0300

    js
    traer imangen de api, falta mejorar, que sea un push up

--- script.js
+++ script.js
@@ -7,9 +7,18 @@ function sendCommand(id) {
       
       const nombre = data.name;
       const edad = data.age;
+      const imagen = `https://cdn.thesimpsonsapi.com/500/character/${id}.webp`;
 
-      alert(`${nombre}\n\nEdad: ${edad}\n`);
+      const container = document.createElement("div");
+      container.className = "info-box";
+      container.innerHTML = `
+        <h2>${nombre}</h2>
+        <img src="${imagen}" alt="${nombre}" />
+        <p><strong>Edad:</strong> ${edad}</p>
+      `;
+      document.body.appendChild(container);
     })
+
     .catch(err => {
       console.error("Error al obtener datos:", err);
       alert("No se pudo obtener la información del personaje.");
